eca5595768b3651f106f775ef371b54302ca04a3
boolbee <boolbee01@gmail.com>
2021-01-30 16:18:40 +0100

    Improve proof of concept about contaniner startup and stopping

--- poc-docker/poc-containers/poc-containers-start-stop.sh
+++ poc-docker/poc-containers/poc-containers-start-stop.sh
@@ -2,58 +2,125 @@
 
 DIR=$(dirname $(readlink -f $0))
 
-source "${DIR}/../../dependencies/downloads/poc-bash-master/includes/print-utils.src"
+source "${DIR}/../utils/docker-utils.src"
 
-CONTAINER_PREFIX="topdemo_$(date '+%Y%m%d')"
+CONTAINER_PREFIX="poc_ubuntu_top$(date '+%Y%m%d')"
 CONTAINER1_NAME="${CONTAINER_PREFIX}_1"
 CONTAINER2_NAME="${CONTAINER_PREFIX}_2"
+CONTAINER3_NAME="${CONTAINER_PREFIX}_3"
+CONTAINER4_NAME="${CONTAINER_PREFIX}_4"
 
-function handleSignal {
-  echo "Caught signal..."
-  echo "Done cleanup... quitting"
-  docker ps -a | grep $CONTAINER_PREFIX
-  if [ $? -eq 0 ]
-  then
-    echo "OK"
-    set -x
-    docker rm -f ${CONTAINER1_NAME}
-    docker rm -f ${CONTAINER2_NAME}
-  else
-    echo "KO"
-  fi
+
+function initialize {
+  print_info "Preparing poc environment..."
+  trap handleTermSignal INT QUIT TERM KILL
+  cleanup
+}
+
+function handleTermSignal {
+  xtrace off
+  print_warn "Handling termination signal..."
+  cleanup
   exit 1
 }
 
-echo "Initializing... (PID = $$)"
-trap handleSignal INT QUIT TERM KILL
+function cleanup {  
+  containers=($(docker_utils::getAllContainerIdsByPrefix ${CONTAINER_PREFIX}))
+  echo "Containers: ${containers[@]}"
+  echo "Count: ${#containers[@]}"
+  if [ ${#containers[@]} -gt 0 ]
+  then
+    print_debug "Se necesita limpiar"
+    for containerId in ${containers[@]}
+    do
+      xtrace on
+      docker rm -f $containerId
+      xtrace off
+    done
+  fi
+}
 
-CONTAINER_NAME="topdemo_$(date '+%Y%m%d')"
-
-print_info "Execute containers..."
-set -x
-
-docker run -dit \
+function executeContainers {
+  print_info "Execute containers..."  
+  xtrace on
+  docker run -dit \
     --name ${CONTAINER1_NAME} \
     ubuntu /usr/bin/top -b
 
-docker run -dit \
+  docker run -dit \
+    --name ${CONTAINER3_NAME} \
+    ubuntu /usr/bin/top -b
+
+  docker run -dit \
     --rm \
     --name ${CONTAINER2_NAME} \
     ubuntu /usr/bin/top -b
+  
+  docker run -dit \
+    --rm \
+    --name ${CONTAINER4_NAME} \
+    ubuntu /usr/bin/top -b
 
-set +x
-print_info "Check containers..."
+  xtrace off
+}
 
-set -x
-docker ps -a | grep ${container_name}
+function startContainers {
+  print_info "Start containers..."
+  containers=$(docker_utils::getExitedContainerIdsByPrefix ${CONTAINER_PREFIX})
 
-set +x
-while /bin/true
-do
-  sleep 3
-  echo "Processing..."
-done
+  # args=("$@") 
+  # get number of elements 
+  # ELEMENTS=${#args[@]} 
+  # echo each element in array  
+  # for loop 
+  # for (( i=0;i<$ELEMENTS;i++)); do 
+  # echo ${args[${i}]} 
 
 
+  for containerId in ${containers}
+  do
+    xtrace on
+    docker start ${containerId}
+    xtrace off
+  done
+}
 
+function stopContainers {
+  print_info "Stop containers..."
+  containers=$(docker_utils::getRunningContainerIdsByPrefix ${CONTAINER_PREFIX})
 
+  for containerId in ${containers}
+  do
+    xtrace on
+    docker stop ${containerId}
+    xtrace off
+  done
+}
+
+function main {
+  print_info "$(basename $0) [PID = $$]"
+  initialize
+  executeContainers
+
+  print_info "Check containers status..."
+  docker_utils::showContainersByPrefix ${CONTAINER_PREFIX}
+  stopContainers
+
+  print_info "Check containers status after stopping..."
+  docker_utils::showContainersByPrefix ${CONTAINER_PREFIX}
+  startContainers
+
+  print_info "Check containers status after startup..."
+  docker_utils::showContainersByPrefix ${CONTAINER_PREFIX}
+
+  print_info "Cleanup containers..."
+  cleanup
+
+  print_info "Check containers status after cleanup..."
+  docker_utils::showContainersByPrefix ${CONTAINER_PREFIX}
+
+  print_done "Poc completed successfully "
+  exit 0
+}
+
+main $@
