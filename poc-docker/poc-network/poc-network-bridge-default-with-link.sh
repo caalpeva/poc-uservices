52cad360c0fc25366715fb9093640ca1becf193d
caalpeva <caalpeva@yahoo.es>
2022-02-24 15:36:03 +0100

    Add proof of concept about network bridge by default

---
+++ poc-docker/poc-network/poc-network-bridge-default-with-link.sh
@@ -0,0 +1,123 @@
+#!/bin/bash
+
+DIR=$(dirname $(readlink -f $0))
+
+source "${DIR}/../../dependencies/downloads/poc-bash-master/includes/print-utils.src"
+source "${DIR}/../../dependencies/downloads/poc-bash-master/includes/trace-utils.src"
+source "${DIR}/../../utils/microservices-utils.src"
+source "${DIR}/../utils/docker-utils.src"
+
+CONTAINER_PREFIX="poc_link"
+CONTAINER_MYSQL="${CONTAINER_PREFIX}_mysql"
+CONTAINER_ADMINER="${CONTAINER_PREFIX}_adminer"
+
+CONTAINER_PORT="3306"
+HOST_PORT="3306"
+VOLUMEN_NAME="mysql_data"
+MYSQL_ROOT_PASSWORD="root"
+
+NETWORK_NAME="bridge"
+
+function initialize() {
+  print_info "Preparing poc environment..."
+  setTerminalSignals
+  cleanup
+}
+
+function handleTermSignal() {
+  xtrace off
+  print_warn "Handling termination signal..."
+  cleanup
+  exit 1
+}
+
+function cleanup {
+  print_debug "Cleaning environment..."
+  containers=($(docker_utils::getAllContainerIdsByPrefix ${CONTAINER_PREFIX}))
+  docker_utils::removeContainers ${containers[*]}
+  docker_utils::removeVolumes ${VOLUMEN_NAME}
+}
+
+function executeMysqlContainer {
+  print_info "Execute container ${CONTAINER_MYSQL} with MySQL server"
+  xtrace on
+  docker run -d \
+    --name ${CONTAINER_MYSQL} \
+    -e MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD} \
+    -p ${HOST_PORT}:${CONTAINER_PORT} \
+    -v ${VOLUMEN_NAME}:/var/lib/mysql \
+    -v ${DIR}/scripts/initial:/docker-entrypoint-initdb.d \
+    mysql:5.7.28
+
+  xtrace off
+
+  docker_utils::getContainerMounts ${CONTAINER_MYSQL}
+}
+
+function executeAdminerContainer {
+  print_info "Execute container ${CONTAINER_ADMINER} with adminer"
+  xtrace on
+  docker run -d \
+    --name ${CONTAINER_ADMINER} \
+    --link ${CONTAINER_MYSQL} \
+    -p 8080:8080 \
+    adminer:4.8.1
+
+  xtrace off
+}
+
+function main {
+  print_info "$(basename $0) [PID = $$]"
+  checkArguments $@
+  initialize
+
+  docker_utils::getNetworkList
+  print_info "Show network data: $NETWORK_NAME"
+  docker_utils::networkInspect $NETWORK_NAME
+
+  print_info "Create ${VOLUMEN_NAME} volumen"
+  docker_utils::createVolumen ${VOLUMEN_NAME}
+
+  executeMysqlContainer
+  executeAdminerContainer
+
+  print_info "Check containers status..."
+  docker_utils::showContainersByPrefix ${CONTAINER_PREFIX}
+
+  print_info "Get ip address from containers"
+  MYSQL_IP_ADDRESS=$(docker_utils::getIpAddressFromContainer ${CONTAINER_MYSQL})
+  echo ${MYSQL_IP_ADDRESS}
+
+  echo -e "The containers on the default bridge network can only access other containers \non the same network through their IP addresses or using the --link option considered legacy."
+  checkInteractiveMode
+
+  print_info "Check connection from ${CONTAINER_ADMINER} to ${CONTAINER_MYSQL} by ip address"
+  docker_utils::execContainerPingAsRoot ${CONTAINER_ADMINER} ${MYSQL_IP_ADDRESS}
+  if [ $? -ne 0 ]; then
+    print_error "Poc completed with failure"
+    exit 1
+  fi
+
+  checkInteractiveMode
+  print_info "Check connection from ${CONTAINER_ADMINER} to ${CONTAINER_MYSQL} by name"
+  docker_utils::execContainerPingAsRoot ${CONTAINER_ADMINER} ${CONTAINER_MYSQL}
+  if [ $? -ne 0 ]; then
+    print_error "Poc completed with failure"
+    exit 1
+  fi
+
+  checkInteractiveMode
+  print_info "Check /etc/hosts file from ${CONTAINER_ADMINER}"
+  docker_utils::execContainer ${CONTAINER_ADMINER} "cat /etc/hosts"
+
+  print_info "Check that the ${CONTAINER_ADMINER} container can connect to the ${CONTAINER_MYSQL} container."
+  print_debug "Open a browser and access to http://localhost:8080"
+  print_debug "Note that the name of the server you want to access is the name of the mysql container."
+  checkInteractiveMode
+
+  checkCleanupMode
+  print_done "Poc completed successfully "
+  exit 0
+}
+
+main $@
---
+++ poc-docker/poc-network/poc-network-bridge-default.sh
@@ -0,0 +1,109 @@
+#!/bin/bash
+
+DIR=$(dirname $(readlink -f $0))
+
+source "${DIR}/../../dependencies/downloads/poc-bash-master/includes/print-utils.src"
+source "${DIR}/../../dependencies/downloads/poc-bash-master/includes/trace-utils.src"
+source "${DIR}/../../utils/microservices-utils.src"
+source "${DIR}/../utils/docker-utils.src"
+
+CONTAINER_PREFIX="poc_alpine"
+CONTAINER1_NAME="${CONTAINER_PREFIX}_1"
+CONTAINER2_NAME="${CONTAINER_PREFIX}_2"
+
+NETWORK_NAME="bridge"
+
+function initialize() {
+  print_info "Preparing poc environment..."
+  setTerminalSignals
+  cleanup
+}
+
+function handleTermSignal() {
+  xtrace off
+  print_warn "Handling termination signal..."
+  cleanup
+  exit 1
+}
+
+function cleanup {
+  print_debug "Cleaning environment..."
+  containers=($(docker_utils::getAllContainerIdsByPrefix ${CONTAINER_PREFIX}))
+  docker_utils::removeContainers ${containers[*]}
+}
+
+function executeContainers {
+  print_info "Execute containers..."
+  xtrace on
+  docker run -dit \
+    --name ${CONTAINER1_NAME} \
+    alpine
+
+  docker run -dit \
+    --name ${CONTAINER2_NAME} \
+    alpine
+
+  xtrace off
+}
+
+function main {
+  print_info "$(basename $0) [PID = $$]"
+  checkArguments $@
+  initialize
+
+  docker_utils::getNetworkList
+  print_info "Show network data: $NETWORK_NAME"
+  docker_utils::networkInspect $NETWORK_NAME
+
+  executeContainers
+  print_info "Check containers status..."
+  docker_utils::showContainersByPrefix ${CONTAINER_PREFIX}
+
+  print_info "Get ip address from containers"
+  CONTAINER1_IP_ADDRESS=$(docker_utils::getIpAddressFromContainer ${CONTAINER1_NAME})
+  echo ${CONTAINER1_IP_ADDRESS}
+
+  CONTAINER2_IP_ADDRESS=$(docker_utils::getIpAddressFromContainer ${CONTAINER2_NAME})
+  echo ${CONTAINER2_IP_ADDRESS}
+
+  echo -e "The containers on the default bridge network can only access other containers \non the same network through their IP addresses or using the --link option considered legacy."
+  checkInteractiveMode
+
+  print_info "Check connection from ${CONTAINER1_NAME} to ${CONTAINER2_NAME} by ip address"
+  docker_utils::execContainerPing ${CONTAINER1_NAME} ${CONTAINER2_IP_ADDRESS}
+  if [ $? -ne 0 ]; then
+    print_error "Poc completed with failure"
+    exit 1
+  fi
+
+  checkInteractiveMode
+  print_info "Check connection from ${CONTAINER2_NAME} to ${CONTAINER1_NAME} by ip address"
+  docker_utils::execContainerPing ${CONTAINER2_NAME} ${CONTAINER1_IP_ADDRESS}
+  if [ $? -ne 0 ]; then
+    print_error "Poc completed with failure"
+    exit 1
+  fi
+
+  checkInteractiveMode
+  print_info "Check connection from ${CONTAINER1_NAME} to ${CONTAINER2_NAME} by name"
+  docker_utils::execContainerPing ${CONTAINER1_NAME} ${CONTAINER2_NAME}
+  if [ $? -eq 0 ]; then
+    print_error "Poc completed with failure"
+    exit 1
+  fi
+
+  checkInteractiveMode
+  print_info "Check connection from ${CONTAINER2_NAME} to ${CONTAINER1_NAME} by name"
+  docker_utils::execContainerPing ${CONTAINER2_NAME} ${CONTAINER1_NAME}
+  if [ $? -eq 0 ]; then
+    print_error "Poc completed with failure"
+    exit 1
+  fi
+
+  checkInteractiveMode
+  checkCleanupMode
+  print_done "Poc completed successfully "
+  exit 0
+}
+
+main $@
