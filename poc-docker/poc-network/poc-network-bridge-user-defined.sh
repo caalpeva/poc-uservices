4871c455bb89cb5d6b5b132ad8da4ac72cff3c4c
caalpeva <caalpeva@yahoo.es>
2022-02-24 17:16:09 +0100

    Add new proof of concept about network user-defined

--- poc-docker/poc-network/poc-network-bridge-user-defined.sh
+++ poc-docker/poc-network/poc-network-bridge-user-defined.sh
@@ -7,16 +7,16 @@ source "${DIR}/../../dependencies/downloads/poc-bash-master/includes/trace-utils
 source "${DIR}/../../utils/microservices-utils.src"
 source "${DIR}/../utils/docker-utils.src"
 
-CONTAINER_PREFIX="poc_link"
+CONTAINER_PREFIX="poc_network"
 CONTAINER_MYSQL="${CONTAINER_PREFIX}_mysql"
-CONTAINER_ADMINER="${CONTAINER_PREFIX}_adminer"
+CONTAINER_PHPMYADMIN="${CONTAINER_PREFIX}_phpmyadmin"
 
 MYSQL_ROOT_PASSWORD="root"
 
-ADMINER_CONTAINER_PORT="8080"
-ADMINER_HOST_PORT="8080"
+PHPMYADMIN_CONTAINER_PORT="80"
+PHPMYADMIN_HOST_PORT="8081"
 
-NETWORK_NAME="bridge"
+NETWORK_NAME="poc_network"
 
 function initialize() {
   print_info "Preparing poc environment..."
@@ -35,6 +35,7 @@ function cleanup {
   print_debug "Cleaning environment..."
   containers=($(docker_utils::getAllContainerIdsByPrefix ${CONTAINER_PREFIX}))
   docker_utils::removeContainers ${containers[*]}
+  docker_utils::removeNetwork ${NETWORK_NAME}
 }
 
 function executeMysqlContainer {
@@ -42,6 +43,7 @@ function executeMysqlContainer {
   xtrace on
   docker run -d \
     --name ${CONTAINER_MYSQL} \
+    --network ${NETWORK_NAME}\
     -e MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD} \
     -v ${DIR}/scripts/initial:/docker-entrypoint-initdb.d \
     mysql:5.7.28
@@ -51,14 +53,15 @@ function executeMysqlContainer {
   docker_utils::getContainerMounts ${CONTAINER_MYSQL}
 }
 
-function executeAdminerContainer {
-  print_info "Execute container ${CONTAINER_ADMINER} with adminer"
+function executePhpMyAdminContainer {
+  print_info "Execute container ${CONTAINER_PHPMYADMIN} with phpMyAdmin"
   xtrace on
   docker run -d \
-    --name ${CONTAINER_ADMINER} \
-    --link ${CONTAINER_MYSQL} \
-    -p ${ADMINER_HOST_PORT}:${ADMINER_CONTAINER_PORT} \
-    adminer:4.8.1
+    --name ${CONTAINER_PHPMYADMIN} \
+    --network ${NETWORK_NAME} \
+    -e PMA_ARBITRARY=1 \
+    -p ${PHPMYADMIN_HOST_PORT}:${PHPMYADMIN_CONTAINER_PORT} \
+    phpmyadmin/phpmyadmin:5.1.3
 
   xtrace off
 }
@@ -68,44 +71,41 @@ function main {
   checkArguments $@
   initialize
 
+  print_info "Create ${NETWORK_NAME} network"
+  docker_utils::createNetwork "--subnet 180.128.10.0/24 --gateway 180.128.10.1 -d bridge" ${NETWORK_NAME}
+
   docker_utils::getNetworkList
   print_info "Show network data: $NETWORK_NAME"
   docker_utils::networkInspect $NETWORK_NAME
 
   executeMysqlContainer
-  executeAdminerContainer
+  executePhpMyAdminContainer
 
   print_info "Check containers status..."
   docker_utils::showContainersByPrefix ${CONTAINER_PREFIX}
 
-  print_info "Get ip address from containers"
-  MYSQL_IP_ADDRESS=$(docker_utils::getIpAddressFromContainer ${CONTAINER_MYSQL})
-  echo ${MYSQL_IP_ADDRESS}
-
   echo -e "The containers on the default bridge network can only access other containers \non the same network through their IP addresses or using the --link option considered legacy."
+
+  print_info "Install network utils in container ${CONTAINER_PHPMYADMIN}"
+  xtrace on
+  docker exec -u root ${CONTAINER_PHPMYADMIN} apt-get update > /dev/null
+  docker exec -u root ${CONTAINER_PHPMYADMIN} apt-get install -y iputils-ping > /dev/null
+  xtrace off
   checkInteractiveMode
 
-  print_info "Check connection from ${CONTAINER_ADMINER} to ${CONTAINER_MYSQL} by ip address"
-  docker_utils::execContainerPingAsRoot ${CONTAINER_ADMINER} ${MYSQL_IP_ADDRESS}
+  print_info "Check connection from ${CONTAINER_PHPMYADMIN} to ${CONTAINER_MYSQL} by name"
+  docker_utils::execContainerPingAsRoot ${CONTAINER_PHPMYADMIN} ${CONTAINER_MYSQL}
   if [ $? -ne 0 ]; then
     print_error "Poc completed with failure"
     exit 1
   fi
 
   checkInteractiveMode
-  print_info "Check connection from ${CONTAINER_ADMINER} to ${CONTAINER_MYSQL} by name"
-  docker_utils::execContainerPingAsRoot ${CONTAINER_ADMINER} ${CONTAINER_MYSQL}
-  if [ $? -ne 0 ]; then
-    print_error "Poc completed with failure"
-    exit 1
-  fi
+  print_info "Check /etc/hosts file from ${CONTAINER_PHPMYADMIN}"
+  docker_utils::execContainer ${CONTAINER_PHPMYADMIN} "cat /etc/hosts"
 
-  checkInteractiveMode
-  print_info "Check /etc/hosts file from ${CONTAINER_ADMINER}"
-  docker_utils::execContainer ${CONTAINER_ADMINER} "cat /etc/hosts"
-
-  print_info "Check that the ${CONTAINER_ADMINER} container can connect to the ${CONTAINER_MYSQL} container."
-  print_debug "Open a browser and access to http://localhost:${ADMINER_HOST_PORT}"
+  print_info "Check that the ${CONTAINER_PHPMYADMIN} container can connect to the ${CONTAINER_MYSQL} container."
+  print_debug "Open a browser and access to http://localhost:${PHPMYADMIN_HOST_PORT}"
   print_debug "Note that the name of the server you want to access is the name of the mysql container."
   checkInteractiveMode
 
