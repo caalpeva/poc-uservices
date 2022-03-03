67ca6bd75b0aa1d28ce917c78d4064ef25de791b
caalpeva <caalpeva@yahoo.es>
2022-03-03 15:48:34 +0100

    Add new proof of concept about network by default in docker-compose use.

---
+++ poc-docker/poc-compose/poc-compose-network-default/poc-compose-network-default.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+DIR=$(dirname $(readlink -f $0))
+
+source "${DIR}/../../../dependencies/downloads/poc-bash-master/includes/print-utils.src"
+source "${DIR}/../../../dependencies/downloads/poc-bash-master/includes/trace-utils.src"
+source "${DIR}/../../../utils/microservices-utils.src"
+source "${DIR}/../../utils/docker-utils.src"
+
+CONTAINER_PREFIX="poc_alpine"
+CONTAINER1_NAME="${CONTAINER_PREFIX}_1"
+CONTAINER2_NAME="${CONTAINER_PREFIX}_2"
+
+function initialize() {
+  print_info "Preparing poc environment..."
+  setTerminalSignals
+  cleanup
+}
+
+function handleTermSignal() {
+  xtrace off
+  print_warn "Handling termination signal..."
+  cleanup
+  exit 1
+}
+
+function cleanup {
+  print_debug "Cleaning environment..."
+  docker_utils::composeDown
+}
+
+function main {
+  print_info "$(basename $0) [PID = $$]"
+  checkArguments $@
+  initialize
+
+  print_box "****************" \
+    "" \
+    " - " \
+    " - " \
+    " - "
+  checkInteractiveMode
+
+  print_info "Execute docker-compose"
+  docker_utils::composeUp
+
+  print_info "Check containers status..."
+  docker_utils::showContainersByPrefix ${CONTAINER_PREFIX}
+
+  print_info "Check connection from ${CONTAINER1_NAME} to ${CONTAINER2_NAME} by name"
+  docker_utils::execContainerPingAsRoot ${CONTAINER1_NAME} ${CONTAINER2_NAME}
+  if [ $? -ne 0 ]; then
+    print_error "Poc completed with failure"
+    exit 1
+  fi
+
+  checkInteractiveMode
+  print_info "Check /etc/hosts file from ${CONTAINER1_NAME}"
+  print_debug "No line added in /etc/hosts because network uses DNS and containers can resolve each other by name "
+  docker_utils::execContainer ${CONTAINER1_NAME} "cat /etc/hosts"
+
+  checkCleanupMode
+  print_done "Poc completed successfully "
+  exit 0
+}
+
+main $@
