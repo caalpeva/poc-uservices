0e999694dc34967692f8d65684b3b6c3b7274b28
caalpeva <boolbee01@gmail.com>
2022-06-08 20:59:45 +0200

    Add proof of concept about probes

---
+++ poc-k8s/poc-probes/poc-probe-liveness/poc-probe-liveness.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+DIR=$(dirname $(readlink -f $0))
+
+source "${DIR}/../../../dependencies/downloads/poc-bash-master/includes/print-utils.src"
+source "${DIR}/../../../dependencies/downloads/poc-bash-master/includes/trace-utils.src"
+source "${DIR}/../../../dependencies/downloads/poc-bash-master/includes/progress-bar-utils.src"
+source "${DIR}/../../../utils/microservices-utils.src"
+source "${DIR}/../../../poc-docker/utils/docker.src"
+source "${DIR}/../../utils/kubectl.src"
+
+CONFIGURATION_FILE_POD=${DIR}/pod.yaml
+POD_NAME="poc-probe-liveness"
+
+function initialize() {
+  print_info "Preparing poc environment..."
+  setTerminalSignals
+  cleanup
+}
+
+function handleTermSignal() {
+  xtrace off
+  print_warn "Handling termination signal..."
+  cleanup
+  exit 1
+}
+
+function cleanup {
+  print_debug "Cleaning environment..."
+  kubectl::unapply $CONFIGURATION_FILE_POD
+}
+
+function main {
+  print_info "$(basename $0) [PID = $$]"
+  checkArguments $@
+  initialize
+
+  kubectl::showNodes
+
+  kubectl::apply $CONFIGURATION_FILE_POD
+  kubectl::showPods
+
+  print_info "Wait a while for kubelet to use liveliness probes..."
+  kubectl::waitForPodRestarted $POD_NAME &
+  PID=$!
+  showProgressBar $PID
+  wait $PID
+
+  kubectl::showPods
+  print_debug "Note that when liveness probe fails the pod is restarted"
+
+  checkCleanupMode
+  print_done "Poc completed successfully"
+  exit 0
+}
+
+main $@
---
+++ poc-k8s/poc-probes/poc-probe-readiness/poc-probe-readiness.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+DIR=$(dirname $(readlink -f $0))
+
+source "${DIR}/../../../dependencies/downloads/poc-bash-master/includes/print-utils.src"
+source "${DIR}/../../../dependencies/downloads/poc-bash-master/includes/trace-utils.src"
+source "${DIR}/../../../dependencies/downloads/poc-bash-master/includes/progress-bar-utils.src"
+source "${DIR}/../../../utils/microservices-utils.src"
+source "${DIR}/../../../poc-docker/utils/docker.src"
+source "${DIR}/../../utils/kubectl.src"
+
+CONFIG_DIR=${DIR}/config
+CONFIGFILE_PODS=${CONFIG_DIR}/pods.yaml
+CONFIGFILE_SERVICE=${CONFIG_DIR}/service.yaml
+SERVICE_NAME="poc-probe-readiness"
+POC_LABEL_VALUE=$SERVICE_NAME
+
+function initialize() {
+  print_info "Preparing poc environment..."
+  setTerminalSignals
+  cleanup
+}
+
+function handleTermSignal() {
+  xtrace off
+  print_warn "Handling termination signal..."
+  cleanup
+  exit 1
+}
+
+function cleanup {
+  print_debug "Cleaning environment..."
+  kubectl::unapply $CONFIGFILE_SERVICE $CONFIGFILE_PODS
+}
+
+function main {
+  print_info "$(basename $0) [PID = $$]"
+  checkArguments $@
+  initialize
+
+  kubectl::showNodes
+
+  kubectl::apply $CONFIGFILE_PODS $CONFIGFILE_SERVICE
+  kubectl::waitForPodsByLabel -l "poc=$POC_LABEL_VALUE"
+  kubectl::showPods
+  kubectl::showServices -l "poc=$POC_LABEL_VALUE"
+  kubectl::showEndpointsByService $SERVICE_NAME
+
+  print_info "Wait a while for kubelet to use readiness probes..." && sleep 30
+  print_debug "Check that when readiness probe fails the pod is not restarted but"
+  print_debug "the pod will be marked as not ready and its IP will be removed from the service endpoints"
+  kubectl::showPods
+  kubectl::showServices -l "poc=$POC_LABEL_VALUE"
+  kubectl::showEndpointsByService $SERVICE_NAME
+
+  checkCleanupMode
+  print_done "Poc completed successfully"
+  exit 0
+}
+
+main $@
