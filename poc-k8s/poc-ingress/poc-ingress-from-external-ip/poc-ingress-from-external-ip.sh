ebb4fe0b7d2ea3075c5034a3f22ce892832d51d9
caalpeva <kalpeva@gmail.com>
2022-06-16 22:01:20 +0200

    Add poc about ingress from external ip

---
+++ poc-k8s/poc-ingress/poc-ingress-from-external-ip/poc-ingress-from-external-ip.sh
@@ -0,0 +1,118 @@
+#!/bin/bash
+
+DIR=$(dirname $(readlink -f $0))
+
+source "${DIR}/../../../dependencies/downloads/poc-bash-master/includes/print-utils.src"
+source "${DIR}/../../../dependencies/downloads/poc-bash-master/includes/trace-utils.src"
+source "${DIR}/../../../dependencies/downloads/poc-bash-master/includes/progress-bar-utils.src"
+source "${DIR}/../../../utils/microservices-utils.src"
+source "${DIR}/../../../poc-docker/utils/docker.src"
+source "${DIR}/../../utils/kubectl.src"
+
+CONFIG_DIR=${DIR}/config
+CONFIGFILE_DEPLOYMENT_01=${CONFIG_DIR}/deployment-01.yaml
+CONFIGFILE_DEPLOYMENT_02=${CONFIG_DIR}/deployment-02.yaml
+CONFIGFILE_INGRESS=${CONFIG_DIR}/ingress.yaml
+
+INGRESS_CONTROLLER_NAMESPACE="ingress-nginx"
+INGRESS_SERVICE_NAME="ingress-nginx-controller"
+
+DEPLOYMENT_SERVER_01_NAME="poc-deployment-01"
+DEPLOYMENT_SERVER_02_NAME="poc-deployment-02"
+INGRESS_NAME="poc-ingress-from-external-ip"
+POC_LABEL_VALUE=$INGRESS_NAME
+
+function initialize() {
+  print_info "Preparing poc environment..."
+  setTerminalSignals
+  cleanup
+}
+
+function handleTermSignal() {
+  xtrace off
+  print_warn "Handling termination signal..."
+  cleanup
+  exit 1
+}
+
+function cleanup {
+  print_debug "Cleaning environment..."
+  kubectl::unapply $CONFIGFILE_INGRESS \
+    $CONFIGFILE_DEPLOYMENT_01 \
+    $CONFIGFILE_DEPLOYMENT_02
+}
+
+function main {
+  print_info "$(basename $0) [PID = $$]"
+  checkArguments $@
+  initialize
+
+  kubectl::showNodes
+  kubectl::apply $CONFIGFILE_DEPLOYMENT_01 \
+    $CONFIGFILE_DEPLOYMENT_02 \
+    $CONFIGFILE_INGRESS
+  kubectl::waitForDeployment $DEPLOYMENT_SERVER_01_NAME
+  kubectl::waitForDeployment $DEPLOYMENT_SERVER_02_NAME
+  kubectl::showDeployments -l "poc=$POC_LABEL_VALUE"
+  kubectl::showReplicaSets -l "poc=$POC_LABEL_VALUE"
+  kubectl::showPods -l "poc=$POC_LABEL_VALUE"
+  kubectl::showServices -l "poc=$POC_LABEL_VALUE"
+  kubectl::showEndpointsByLabels -l "poc=$POC_LABEL_VALUE"
+
+  sleep 5 && kubectl::showIngresses
+  kubectl::showIngressesDescription $INGRESS_NAME
+
+  print_info "Extract node port from ingress controller service"
+  NODE_PORT=$(kubectl::getNodePortByService ${INGRESS_SERVICE_NAME} http -n $INGRESS_CONTROLLER_NAMESPACE)
+  echo $NODE_PORT
+  checkInteractiveMode
+
+  print_info "Check access to each application"
+  print_debug "Note that the port has been enabled on all nodes"
+  NODE_ADDRESSES=$(kubectl::getNodeAddresses)
+  for NODE_ADDRESS in ${NODE_ADDRESSES[@]}
+  do
+    executeCurl http://$NODE_ADDRESS:$NODE_PORT/webapp-server-01/echo
+    if [ $? -ne 0 ]
+    then
+      print_error "Http server from $NODE_ADDRESS:$NODE_PORT is not available"
+      SERVER_AVAILABLE=false
+    fi
+    executeCurl http://$NODE_ADDRESS:$NODE_PORT/webapp-server-02/
+    if [ $? -ne 0 ]
+    then
+      print_error "Http server from $NODE_ADDRESS:$NODE_PORT is not available"
+      SERVER_AVAILABLE=false
+    fi
+  done
+  checkInteractiveMode
+
+  print_info "Check access from traditional port 80 via proxy"
+  print_debug "Only if a proxy is used on the master node to redirect traffic from the traditional"
+  print_debug "ports (80 for HTTP and 443 for HTTPS) to the ports exposed by the ingress controller"
+  print_debug "Note that the port 80 has only been enabled on master node"
+  NODE_PORT=80
+  NODE_ADDRESSES=$(kubectl::getNodeAddresses)
+  for NODE_ADDRESS in ${NODE_ADDRESSES[@]}
+  do
+    executeCurl http://$NODE_ADDRESS:$NODE_PORT/webapp-server-01/echo
+    if [ $? -ne 0 ]
+    then
+      print_warn "Http server from $NODE_ADDRESS:$NODE_PORT is not available"
+      SERVER_AVAILABLE=false
+    fi
+    executeCurl http://$NODE_ADDRESS:$NODE_PORT/webapp-server-02/
+    if [ $? -ne 0 ]
+    then
+      print_warn "Http server from $NODE_ADDRESS:$NODE_PORT is not available"
+      SERVER_AVAILABLE=false
+    fi
+  done
+  checkInteractiveMode
+
+  checkCleanupMode
+  print_done "Poc completed successfully"
+  exit 0
+}
+
+main $@
---
+++ poc-k8s/poc-ingress/poc-install-ingress-controller.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+DIR=$(dirname $(readlink -f $0))
+
+source "${DIR}/../../dependencies/downloads/poc-bash-master/includes/print-utils.src"
+source "${DIR}/../../dependencies/downloads/poc-bash-master/includes/trace-utils.src"
+source "${DIR}/../../dependencies/downloads/poc-bash-master/includes/progress-bar-utils.src"
+source "${DIR}/../../utils/microservices-utils.src"
+source "${DIR}/../../poc-docker/utils/docker.src"
+source "${DIR}/../utils/kubectl.src"
+
+CONFIGFILE_INGRESS_CONTROLLER=${DIR}/components/nginx-ingress-controller.yaml
+NAMESPACE=ingress-nginx
+
+function initialize() {
+  print_info "Preparing poc environment..."
+  setTerminalSignals
+  cleanup
+}
+
+function handleTermSignal() {
+  xtrace off
+  print_warn "Handling termination signal..."
+  cleanup
+  exit 1
+}
+
+function cleanup {
+  print_debug "Cleaning environment..."
+  kubectl::unapply $CONFIGFILE_INGRESS_CONTROLLER
+}
+
+function main {
+  print_info "$(basename $0) [PID = $$]"
+  checkArguments $@
+  initialize
+
+  print_box "INGRESS CONTROLLER INSTALLATION" \
+    "" \
+    " - Installs the ingress controller with nginx that allows..."
+  checkInteractiveMode
+
+  kubectl::showNodes
+  kubectl::apply $CONFIGFILE_INGRESS_CONTROLLER
+
+  print_info "Wait for ingress-controller..."
+  print_debug "The first time the ingress controller starts, two Jobs create the SSL Certificate"
+  print_debug "used by the admission webhook. This can cause an initial delay of up to two minutes"
+  print_debug "until it is possible to create and validate Ingress definitions."
+  xtrace on
+  kubectl wait --namespace ingress-nginx \
+    --for=condition=ready pod \
+    -l app.kubernetes.io/component=controller \
+    --timeout=120s
+  xtrace off
+  checkInteractiveMode
+
+  print_info "Check ingress controller version"
+  POD_NAME=$(kubectl::getRunningPods -n $NAMESPACE -l app.kubernetes.io/name=ingress-nginx)
+  xtrace on
+  kubectl exec $POD_NAME -n $NAMESPACE -- /nginx-ingress-controller --version
+  xtrace off
+
+  checkCleanupMode
+  print_done "Poc completed successfully"
+  exit 0
+}
+
+main $@
