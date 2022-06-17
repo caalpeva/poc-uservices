6a2c630113e8dc9f543494e2c5d30be62fa2b96e
caalpeva <kalpeva@gmail.com>
2022-06-17 12:49:40 +0200

    Improve poc about ingress from external ip

--- poc-k8s/poc-ingress/poc-ingress-from-external-ip/poc-ingress-from-external-ip.sh
+++ poc-k8s/poc-ingress/poc-ingress-from-external-ip/poc-ingress-from-external-ip.sh
@@ -42,6 +42,27 @@ function cleanup {
     $CONFIGFILE_DEPLOYMENT_02
 }
 
+function checkServicesAvailable {
+  NODE_PORT=$1
+  NODE_ADDRESSES=$(kubectl::getMasterNodeAddresses)
+  for NODE_ADDRESS in ${NODE_ADDRESSES[@]}
+  do
+    executeCurl http://${NODE_ADDRESS}:${NODE_PORT}/webapp-server-01/echo
+    if [ $? -ne 0 ]
+    then
+      print_error "Http server from ${NODE_ADDRESS}:${NODE_PORT} is not available"
+      SERVER_AVAILABLE=false
+    fi
+    executeCurl http://${NODE_ADDRESS}:${NODE_PORT}/webapp-server-02/
+    if [ $? -ne 0 ]
+    then
+      print_error "Http server from ${NODE_ADDRESS}:${NODE_PORT} is not available"
+      SERVER_AVAILABLE=false
+    fi
+  done
+  checkInteractiveMode
+}
+
 function main {
   print_info "$(basename $0) [PID = $$]"
   checkArguments $@
@@ -49,8 +70,7 @@ function main {
 
   kubectl::showNodes
   kubectl::apply $CONFIGFILE_DEPLOYMENT_01 \
-    $CONFIGFILE_DEPLOYMENT_02 \
-    $CONFIGFILE_INGRESS
+    $CONFIGFILE_DEPLOYMENT_02
   kubectl::waitForDeployment $DEPLOYMENT_SERVER_01_NAME
   kubectl::waitForDeployment $DEPLOYMENT_SERVER_02_NAME
   kubectl::showDeployments -l "poc=$POC_LABEL_VALUE"
@@ -59,56 +79,27 @@ function main {
   kubectl::showServices -l "poc=$POC_LABEL_VALUE"
   kubectl::showEndpointsByLabels -l "poc=$POC_LABEL_VALUE"
 
-  sleep 5 && kubectl::showIngresses
+  print_info "Check that the applications are still not accessible from the external IP (master node address)"
+  print_debug "Note that the ingress controller is working correctly. The '404 not found' message indicates"
+  print_debug "that ingress has not been configured for the application services."
+  checkServicesAvailable
+
+  kubectl::apply $CONFIGFILE_INGRESS && sleep 5
+  kubectl::showIngresses
   kubectl::showIngressesDescription $INGRESS_NAME
 
   print_info "Extract node port from ingress controller service"
   NODE_PORT=$(kubectl::getNodePortByService ${INGRESS_SERVICE_NAME} http -n $INGRESS_CONTROLLER_NAMESPACE)
-  echo $NODE_PORT
   checkInteractiveMode
 
-  print_info "Check access to each application via external ip"
-  print_debug "Note that the port has been enabled on all nodes"
-  NODE_ADDRESSES=$(kubectl::getNodeAddresses)
-  for NODE_ADDRESS in ${NODE_ADDRESSES[@]}
-  do
-    executeCurl http://$NODE_ADDRESS:$NODE_PORT/webapp-server-01/echo
-    if [ $? -ne 0 ]
-    then
-      print_error "Http server from $NODE_ADDRESS:$NODE_PORT is not available"
-      SERVER_AVAILABLE=false
-    fi
-    executeCurl http://$NODE_ADDRESS:$NODE_PORT/webapp-server-02/
-    if [ $? -ne 0 ]
-    then
-      print_error "Http server from $NODE_ADDRESS:$NODE_PORT is not available"
-      SERVER_AVAILABLE=false
-    fi
-  done
-  checkInteractiveMode
+  print_info "Check that now the applications are accessible from the external IP (master node address)"
+  checkServicesAvailable $NODE_PORT
 
   print_info "Check access from traditional port 80 via proxy"
   print_debug "Only if a proxy is used on the master node to redirect traffic from the traditional"
   print_debug "ports (80 for HTTP and 443 for HTTPS) to the ports exposed by the ingress controller"
-  print_debug "Note that the port 80 has only been enabled on master node"
-  NODE_PORT=80
-  NODE_ADDRESSES=$(kubectl::getNodeAddresses)
-  for NODE_ADDRESS in ${NODE_ADDRESSES[@]}
-  do
-    executeCurl http://$NODE_ADDRESS:$NODE_PORT/webapp-server-01/echo
-    if [ $? -ne 0 ]
-    then
-      print_warn "Http server from $NODE_ADDRESS:$NODE_PORT is not available"
-      SERVER_AVAILABLE=false
-    fi
-    executeCurl http://$NODE_ADDRESS:$NODE_PORT/webapp-server-02/
-    if [ $? -ne 0 ]
-    then
-      print_warn "Http server from $NODE_ADDRESS:$NODE_PORT is not available"
-      SERVER_AVAILABLE=false
-    fi
-  done
-  checkInteractiveMode
+  #print_debug "Note that the port 80 has only been enabled on master node"
+  checkServicesAvailable 80
 
   checkCleanupMode
   print_done "Poc completed successfully"
