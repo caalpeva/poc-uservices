a5c081b1eb03820b5746159363ae373f0d97a435
caalpeva <boolbee01@gmail.com>
2022-11-20 11:49:22 +0100

    Add poc about signaled workflow and poll results

--- poc-docker/poc-compose/poc-compose-app/poc-compose-cadence/app-multi/team.boolbee.poc.cadence/poc-cadence-greeting/src/main/java/team/boolbee/poc/cadence/entities/CadenceHelper.java
+++ poc-docker/poc-compose/poc-compose-app/poc-compose-cadence/app-multi/team.boolbee.poc.cadence/poc-cadence-greeting/src/main/java/team/boolbee/poc/cadence/entities/CadenceHelper.java
@@ -1,14 +1,20 @@
 package team.boolbee.poc.cadence.entities;
 
+import com.uber.cadence.*;
 import com.uber.cadence.client.WorkflowClient;
 import com.uber.cadence.client.WorkflowClientOptions;
+import com.uber.cadence.converter.DataConverter;
 import com.uber.cadence.serviceclient.ClientOptions;
 import com.uber.cadence.serviceclient.IWorkflowService;
 import com.uber.cadence.serviceclient.WorkflowServiceTChannel;
 import com.uber.cadence.worker.Worker;
 import com.uber.cadence.worker.WorkerFactory;
 import com.uber.cadence.workflow.Workflow;
+import org.apache.thrift.TException;
 import org.slf4j.Logger;
+
+import java.nio.charset.Charset;
+import java.util.Arrays;
 
 public class CadenceHelper {
     private static Logger logger = Workflow.getLogger(CadenceHelper.class);
@@ -29,14 +35,88 @@ public class CadenceHelper {
 
     // Get worker to poll the task list.
     public static void startOneWorker(WorkflowClient workflowClient,
-                            String taskList,
-                            Class<?>[] workflowImplementationTypes,
-                            Object[] activitiesImplementations) {
+                                      String taskList,
+                                      Class<?>[] workflowImplementationTypes,
+                                      Object[] activitiesImplementations) {
         WorkerFactory factory = WorkerFactory.newInstance(workflowClient);
         Worker worker = factory.newWorker(taskList);
         worker.registerWorkflowImplementationTypes(workflowImplementationTypes);
         worker.registerActivitiesImplementations(activitiesImplementations);
         // Start all workers created by this factory
         factory.start();
+    }
+
+    public static SignaledWorkflowStatus signalAndWait(WorkflowClient workflowClient,
+                                String domain,
+                                String workflowId,
+                                String runId,
+                                Runnable signalOperation,
+                                DataConverter dataConverter,
+                                String signalName,
+                                Object... signalArgs) throws Exception {
+        final byte[] signalData = dataConverter.toData(signalArgs);
+        SignaledWorkflowStatus workflowStatus = new SignaledWorkflowStatus();
+
+        // get the current eventID
+        WorkflowExecution execution = new WorkflowExecution();
+        execution.setWorkflowId(workflowId);
+        execution.setRunId(runId);
+
+        DescribeWorkflowExecutionRequest request = new DescribeWorkflowExecutionRequest();
+        request.setDomain(domain);
+        request.setExecution(execution);
+
+        DescribeWorkflowExecutionResponse response = workflowClient.getService().DescribeWorkflowExecution(request);
+        long currentEventId = response.workflowExecutionInfo.historyLength;
+        workflowStatus.setRunId(response.workflowExecutionInfo.execution.runId);
+
+        // send signal
+        signalOperation.run();
+
+        // Poll history starting from currentEventId,
+        // then wait until the signal is received, and then wait until it's
+        // processed(decisionTaskCompleted)
+        workflowStatus.setSignalReceived(false);
+        workflowStatus.setSignalProcessed(false);
+        workflowStatus.setWorkflowRunning(!response.workflowExecutionInfo.isSetCloseStatus());
+
+        while (workflowStatus.isWorkflowRunning() && !workflowStatus.isSignalProcessed()) {
+            GetWorkflowExecutionHistoryRequest historyRequest = new GetWorkflowExecutionHistoryRequest();
+            historyRequest.setDomain(domain);
+            historyRequest.setExecution(execution);
+            historyRequest.setWaitForNewEvent(true);
+            String token = String.format("{\"RunID\":\"%s\",\"FirstEventID\":0,\"NextEventID\":%d,\"IsWorkflowRunning\":true,\"PersistenceToken\":null,\"TransientDecision\":null,\"BranchToken\":null}",
+                    workflowStatus.getRunId(), currentEventId + 1);
+            historyRequest.setNextPageToken(token.getBytes(Charset.defaultCharset()));
+
+            GetWorkflowExecutionHistoryResponse historyResponse = workflowClient.getService().GetWorkflowExecutionHistory(historyRequest);
+            token = new String(historyResponse.getNextPageToken(), Charset.defaultCharset());
+            workflowStatus.setWorkflowRunning(token.contains("\"IsWorkflowRunning\":true"));
+
+            for (HistoryEvent event : historyResponse.history.events) {
+                if (!workflowStatus.isSignalReceived()) {
+                    // wait for signal received
+                    if (event.getEventType() == EventType.WorkflowExecutionSignaled) {
+                        final byte[] eventSignalData = event.getWorkflowExecutionSignaledEventAttributes().getInput();
+                        final String eventSignalName = event.getWorkflowExecutionSignaledEventAttributes().getSignalName();
+                        if (Arrays.equals(eventSignalData, signalData) && eventSignalName.equals(signalName)) {
+                            workflowStatus.setSignalReceived(true);
+                        } else if (Arrays.equals(eventSignalData, signalData) || eventSignalName.equals(signalName)) {
+                            System.out.println(
+                                    "[WARN] either signal event data or signalName doesn't match, is the signalArgs and signalName correct?");
+                        }
+                    }
+                } else {
+                    // signal is received, now wait for first decision task complete
+                    if (event.getEventType() == EventType.DecisionTaskCompleted) {
+                        workflowStatus.setSignalProcessed(true);
+                        break;
+                    }
+                }
+                currentEventId = event.getEventId();
+            } // for
+        } // while
+
+        return workflowStatus;
     }
 }
---
+++ poc-docker/poc-compose/poc-compose-app/poc-compose-cadence/app-multi/team.boolbee.poc.cadence/poc-cadence-greeting/src/main/java/team/boolbee/poc/cadence/starters/GreetingSignaledAndPollResponseWorkflowStarter.java
@@ -0,0 +1,78 @@
+package team.boolbee.poc.cadence.starters;
+
+import com.uber.cadence.DescribeWorkflowExecutionRequest;
+import com.uber.cadence.DescribeWorkflowExecutionResponse;
+import com.uber.cadence.SearchAttributes;
+import com.uber.cadence.WorkflowExecution;
+import com.uber.cadence.client.WorkflowClient;
+import com.uber.cadence.client.WorkflowOptions;
+import com.uber.cadence.converter.JsonDataConverter;
+import com.uber.cadence.workflow.Workflow;
+import com.uber.cadence.workflow.WorkflowUtils;
+import org.apache.commons.lang.RandomStringUtils;
+import org.apache.thrift.TException;
+import org.slf4j.Logger;
+import team.boolbee.poc.cadence.entities.CadenceHelper;
+import team.boolbee.poc.cadence.entities.SignaledWorkflowStatus;
+import team.boolbee.poc.cadence.entities.workflows.GreetingWorkflowWithSignals;
+import team.boolbee.poc.cadence.entities.workflows.GreetingWorkflowWithSignalsAndPollResponse;
+import team.boolbee.poc.cadence.entities.workflows.IGreetingWorkflowWithSignals;
+
+import java.time.Duration;
+import java.util.List;
+
+import static team.boolbee.poc.cadence.entities.CadenceConstants.DOMAIN;
+
+public class GreetingSignaledAndPollResponseWorkflowStarter {
+    private static Logger logger = Workflow.getLogger(GreetingSignaledAndPollResponseWorkflowStarter.class);
+
+    public static final String TASK_LIST = "poc-tl-greeting-with-signals";
+
+    public static void main(String[] args) throws Exception {
+        var workflowClient = CadenceHelper.createDefaultWorkflowClient(DOMAIN);
+        CadenceHelper.startOneWorker(workflowClient, TASK_LIST, new Class<?>[]{GreetingWorkflowWithSignalsAndPollResponse.class}, new Object[]{});
+
+        // In a real application use a business ID like customer ID or order ID
+        String workflowId = RandomStringUtils.randomAlphabetic(10);
+
+        // Get a workflow stub using the same task list the worker uses.
+        IGreetingWorkflowWithSignals workflow = workflowClient.newWorkflowStub(IGreetingWorkflowWithSignals.class, new WorkflowOptions.Builder().setTaskList(TASK_LIST).setWorkflowId(workflowId).setExecutionStartToCloseTimeout(Duration.ofSeconds(30)).build());
+
+        // Start workflow asynchronously to not use another thread to signal.
+        WorkflowClient.start(workflow::getGreetings);
+        // After start for getGreeting returns, the workflow is guaranteed to be started.
+        // So we can send a signal to it using the workflow stub.
+        // This workflow keeps receiving signals until exit is called
+        String signal = "World";
+
+        final SignaledWorkflowStatus workflowStatus = CadenceHelper.signalAndWait(workflowClient, DOMAIN, workflowId, "", () -> {
+            workflow.waitForName(signal); // sends waitForName signal
+        }, JsonDataConverter.getInstance(), "IGreetingWorkflowWithSignals::receiveName", signal);
+
+        System.out.printf("result: isReceived: %b, isProccessed: %b, isRunning: %b, runID: %s \n",
+                workflowStatus.isSignalReceived(),
+                workflowStatus.isSignalProcessed(),
+                workflowStatus.isWorkflowRunning(),
+                workflowStatus.getRunId());
+
+        if (workflowStatus.isSignalProcessed()) {
+            // Get results from search attribute `CustomKeywordField`
+            WorkflowExecution execution = new WorkflowExecution();
+            execution.setWorkflowId(workflowId);
+            execution.setRunId(workflowStatus.getRunId()); // make sure to sure the same runID in case the current run changes
+
+            DescribeWorkflowExecutionRequest request = new DescribeWorkflowExecutionRequest();
+            request.setDomain(DOMAIN);
+            request.setExecution(execution);
+
+            DescribeWorkflowExecutionResponse resp = workflowClient.getService().DescribeWorkflowExecution(request);
+            SearchAttributes searchAttributes = resp.workflowExecutionInfo.getSearchAttributes();
+            String keyword = WorkflowUtils.getValueFromSearchAttributes(searchAttributes, "CustomKeywordField", String.class);
+            System.out.printf("Signal result is: %s\n", keyword);
+        } else {
+            System.out.printf("No result because signal was not processed");
+        }
+
+        System.exit(0);
+    }
+}
