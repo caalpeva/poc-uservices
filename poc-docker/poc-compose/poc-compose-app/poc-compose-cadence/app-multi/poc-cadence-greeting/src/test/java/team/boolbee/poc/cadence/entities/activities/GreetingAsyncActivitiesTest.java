9cee4cf534c5c39d7d6fc06ce040bd7f5daf83af
caalpeva <boolbee01@gmail.com>
2022-12-09 17:31:36 +0100

    Add more unit tests

--- poc-docker/poc-compose/poc-compose-app/poc-compose-cadence/app-multi/poc-cadence-greeting/src/test/java/team/boolbee/poc/cadence/entities/activities/GreetingAsyncActivitiesTest.java
+++ poc-docker/poc-compose/poc-compose-app/poc-compose-cadence/app-multi/poc-cadence-greeting/src/test/java/team/boolbee/poc/cadence/entities/activities/GreetingAsyncActivitiesTest.java
@@ -8,17 +8,15 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import team.boolbee.poc.cadence.entities.workflows.GreetingWorkflow;
+import team.boolbee.poc.cadence.entities.workflows.GreetingWorkflowWithAsyncActivity;
 import team.boolbee.poc.cadence.entities.workflows.IGreetingWorkflow;
-
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
 
-public class GreetingActivityCompletionTest {
+public class GreetingAsyncActivitiesTest {
 
-    private  final String TASK_LIST = "poc-tl-greeting-with-activity-completion-test";
+    private  final String TASK_LIST = "poc-tl-greeting-with-async-activities-test";
     private TestWorkflowEnvironment testWorkflowEnvironment;
     private WorkflowClient workflowClient;
     private Worker worker;
@@ -27,7 +25,7 @@ public class GreetingActivityCompletionTest {
     public void setUp() {
         testWorkflowEnvironment = TestWorkflowEnvironment.newInstance();
         worker = testWorkflowEnvironment.newWorker(TASK_LIST);
-        worker.registerWorkflowImplementationTypes(GreetingWorkflow.class);
+        worker.registerWorkflowImplementationTypes(GreetingWorkflowWithAsyncActivity.class);
         workflowClient = testWorkflowEnvironment.newWorkflowClient();
     }
 
@@ -37,9 +35,8 @@ public class GreetingActivityCompletionTest {
     }
 
     @Test
-    public void testGreetingWithDefaultActivities() throws ExecutionException, InterruptedException {
-        worker.registerActivitiesImplementations(new GreetingActivitiesWithCompletion(
-                workflowClient.newActivityCompletionClient()));
+    public void testGreetingWithDefaultActivities() {
+        worker.registerActivitiesImplementations(new GreetingActivities());
         testWorkflowEnvironment.start();
 
         // Get a workflow stub using the same task list the worker uses.
@@ -49,15 +46,16 @@ public class GreetingActivityCompletionTest {
                         .setTaskList(TASK_LIST)
                         .build());
 
-        // Execute a workflow asynchronously and wait for workflow completion.
-        CompletableFuture<String> completableFuture = WorkflowClient.execute(workflow::getGreeting, "World");
-        assertEquals("Hello World!", completableFuture.get());
+        // Execute a workflow waiting for it to complete.
+        String greeting = workflow.getGreeting("World");
+        assertEquals("Hello World!\nBye World!", greeting);
     }
 
     @Test
-    public void testGreetingWithMockActivities() throws ExecutionException, InterruptedException {
-        GreetingActivitiesWithCompletion activities = mock(GreetingActivitiesWithCompletion.class);
+    public void testGreetingWithMockActivities() {
+        GreetingActivities activities = mock(GreetingActivities.class);
         when(activities.composeGreeting("Hello", "World")).thenReturn("Hello World!");
+        when(activities.composeGreeting("Bye", "World")).thenReturn("Bye World!");
         worker.registerActivitiesImplementations(activities);
         testWorkflowEnvironment.start();
 
@@ -68,10 +66,10 @@ public class GreetingActivityCompletionTest {
                         .setTaskList(TASK_LIST)
                         .build());
 
-        // Execute a workflow asynchronously and wait for workflow completion.
-        CompletableFuture<String> completableFuture = WorkflowClient.execute(workflow::getGreeting, "World");
-        assertEquals("Hello World!", completableFuture.get());
+        // Execute a workflow waiting for it to complete.
+        String greeting = workflow.getGreeting("World");
+        assertEquals("Hello World!\nBye World!", greeting);
 
-        verify(activities, times(1)).composeGreeting("Hello", "World");
+        verify(activities, times(2)).composeGreeting(anyString(), anyString());
     }
 }
