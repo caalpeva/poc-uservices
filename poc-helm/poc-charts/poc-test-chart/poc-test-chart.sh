d9dbd392df72e1178ac1c5e2c30e2d5da0bbc44d
caalpeva <kalpeva@gmail.com>
2023-05-06 17:24:04 +0200

    Add poc about chart testing

--- poc-helm/poc-charts/poc-test-chart/poc-test-chart.sh
+++ poc-helm/poc-charts/poc-test-chart/poc-test-chart.sh
@@ -17,14 +17,12 @@ NAMESPACE="poc-charts"
 CHARTS_DIRECTORY="${DIR}/charts"
 TEMPLATE_DIRECTORY="${DIR}/charts/${CHART_NAME}/templates"
 
-CHART_NAME="mysql"
+CHART_NAME="apache"
 CHART_RELEASE="poc-$CHART_NAME"
-SERVICE_NAME=$CHART_RELEASE
-SECRET_NAME=$CHART_RELEASE
+SERVICE_NAME="poc-$CHART_NAME"
 
-MYSQL_DATABASE="TENNIS"
-
-LOCAL_PORT=3306
+LABELS="app.kubernetes.io/name=$CHART_NAME,app.kubernetes.io/instance=$CHART_RELEASE"
+LOCAL_PORT=8080
 
 #############
 # FUNCTIONS #
@@ -46,66 +44,37 @@ function handleTermSignal() {
 
 function cleanup() {
   print_debug "Cleaning environment..."
+  if [ -n "$PORT_FORWARD_PID" ]; then
+    print_info "Kill the execution of the port-forward command"
+    evalCommand kill -9 $PORT_FORWARD_PID
+  fi
   helm::uninstallChart $CHART_RELEASE --namespace $NAMESPACE
   kubectl delete ns $NAMESPACE
 }
-
-function showDatabase {
-  POD_NAME=$1
-  MYSQL_ROOT_PASSWORD=$2
-  xtrace on
-  kubectl -n $NAMESPACE exec $POD_NAME -- \
-    mysql -uroot -p${MYSQL_ROOT_PASSWORD} ${MYSQL_DATABASE} \
-    --table -e "select * from COUNTRIES"
-
-  kubectl -n $NAMESPACE exec $POD_NAME -- \
-    mysql -uroot -p${MYSQL_ROOT_PASSWORD} ${MYSQL_DATABASE} \
-    --table -e "select * from PLAYERS"
-  xtrace off
-  sleep 1
-
-  checkInteractiveMode
-}
-
-function updateDatabase {
-  xtrace on
-  kubectl -n $NAMESPACE exec $1 -- \
-    mysql -uroot -p${MYSQL_ROOT_PASSWORD} ${MYSQL_DATABASE} \
-    -e "source /opt/update.sql"
-  xtrace off
-  sleep 1
-
-  checkInteractiveMode
-}
-
 
 function main() {
   print_debug "$(basename $0) [PID = $$]"
   checkArguments $@
   initialize
 
-  print_box "GENERATE K8S MANIFESTS VIA TEMPLATE" \
+  print_box "CHART TESTING" \
     "" \
-    " - Proof of concept about k8s manifests creation from templates"
+    " - Proof of concept about chart testing"
   checkInteractiveMode
 
   kubectl::showNodes
 
   helm::templateFiles "${CHARTS_DIRECTORY}/$CHART_NAME" \
-    "templates/configmap-scripts.yaml"
+    "templates/tests/test-connection.yaml"
 
-  helm::templateFiles "${CHARTS_DIRECTORY}/$CHART_NAME" \
-    "templates/secrets.yaml"
-
-  helm::templateFiles "${CHARTS_DIRECTORY}/$CHART_NAME" \
-    "templates/deployment.yaml"
-
-  helm::installChart $CHART_RELEASE "${CHARTS_DIRECTORY}/$CHART_NAME" \
+  helm::installChartSilently $CHART_RELEASE "${CHARTS_DIRECTORY}/$CHART_NAME" \
     --namespace $NAMESPACE --create-namespace \
     --wait
 
   print_info "Show chart instance"
   helm::showChartReleasesByPrefix $CHART_RELEASE -n $NAMESPACE
+
+  helm::testChart $CHART_RELEASE --namespace $NAMESPACE
 
   kubectl::showDeployments -n $NAMESPACE
   kubectl::showReplicaSets -n $NAMESPACE
@@ -113,19 +82,32 @@ function main() {
   kubectl::showServices -n $NAMESPACE
   kubectl::showEndpointsByService $SERVICE_NAME -n $NAMESPACE
 
-  print_info "Decode mysql-root-password value"
-  MYSQL_ROOT_PASSWORD=$(kubectl::decodeSecretByKey ${SECRET_NAME} "mysql-root-password" -n $NAMESPACE)
-  echo $MYSQL_ROOT_PASSWORD
+  print_info "Extract port from service"
+  SERVICE_PORT=$(kubectl::getPortByService ${SERVICE_NAME} http -n $NAMESPACE)
+  checkInteractiveMode
 
-  POD_NAME=$(kubectl get pods --namespace poc-charts -o jsonpath="{.items[0].metadata.name}")
-  print_info "Show database"
-  showDatabase ${POD_NAME} ${MYSQL_ROOT_PASSWORD}
+  print_info "Forward local port to service port"
+  evalCommand kubectl --namespace poc-charts port-forward service/${SERVICE_NAME} --address 0.0.0.0 ${LOCAL_PORT}:${SERVICE_PORT} "&"
+  PORT_FORWARD_PID=$!
+  checkInteractiveMode
 
-  print_info "Update database"
-  updateDatabase ${POD_NAME} ${MYSQL_ROOT_PASSWORD}
+  sleep 3
+  SERVER_AVAILABLE=true
+  print_info "Visit http://localhost:${LOCAL_PORT} to check also manually this application"
+  print_debug "Make requests from local port..."
+  executeCurl http://localhost:$LOCAL_PORT
+  if [ $? -ne 0 ]
+  then
+    print_error "Http server is not available"
+    SERVER_AVAILABLE=false
+  fi
+  checkInteractiveMode
 
-  print_info "Show database after update data"
-  showDatabase ${POD_NAME} ${MYSQL_ROOT_PASSWORD}
+  checkCleanupMode
+  if [ $SERVER_AVAILABLE = false ]; then
+    print_error "Poc completed with failure"
+    exit 1
+  fi
 
   checkCleanupMode
   print_done "Poc completed successfully"
