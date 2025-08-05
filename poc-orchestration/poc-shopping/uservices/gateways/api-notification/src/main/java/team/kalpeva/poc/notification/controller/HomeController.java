ac40b3bd358d6b056f0043a292b0a01ca232ea28
caalpeva <boolbee01@gmail.com>
2025-08-05 10:32:16 +0200

    Refactor modules

---
+++ poc-orchestration/poc-shopping/uservices/gateways/api-notification/src/main/java/team/kalpeva/poc/notification/controller/HomeController.java
@@ -0,0 +1,13 @@
+package team.kalpeva.poc.notification.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "redirect:/swagger-ui/index.html";
+    }
+}
