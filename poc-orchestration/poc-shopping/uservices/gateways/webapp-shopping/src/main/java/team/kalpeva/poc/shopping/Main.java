bc0e3da8c1f24c88603fb8ecd61219bb9d13472f
caalpeva <boolbee01@gmail.com>
2025-10-04 19:48:37 +0200

    Add prometheus config

--- poc-orchestration/poc-shopping/uservices/gateways/webapp-shopping/src/main/java/team/kalpeva/poc/shopping/Main.java
+++ poc-orchestration/poc-shopping/uservices/gateways/webapp-shopping/src/main/java/team/kalpeva/poc/shopping/Main.java
@@ -1,6 +1,10 @@
 package team.kalpeva.poc.shopping;
 
 import io.vertx.core.Vertx;
+import io.vertx.core.VertxOptions;
+import io.vertx.core.http.HttpServerOptions;
+import io.vertx.micrometer.MicrometerMetricsOptions;
+import io.vertx.micrometer.VertxPrometheusOptions;
 import lombok.extern.slf4j.Slf4j;
 import team.kalpeva.poc.shopping.di.DaggerDiComponent;
 
@@ -8,7 +12,17 @@ import team.kalpeva.poc.shopping.di.DaggerDiComponent;
 public class Main {
 
     public static void main (String[] args) {
-        Vertx vertx = Vertx.vertx();
+        var vertxPrometheusOptions =  new VertxPrometheusOptions()
+                .setStartEmbeddedServer(true)
+                .setEmbeddedServerOptions(new HttpServerOptions().setPort(9091))
+                //.setEmbeddedServerEndpoint("/metrics/vertx")
+                .setEnabled(true);
+
+        Vertx vertx = Vertx.vertx(new VertxOptions()
+                .setMetricsOptions(new MicrometerMetricsOptions()
+                        .setPrometheusOptions(vertxPrometheusOptions)
+                        .setEnabled(true)));
+
         vertx.deployVerticle(DaggerDiComponent.create().mainVerticle())
                 .onSuccess(id -> log.info("✅ Verticle deployed successfully, ID: {}", id))
                 .onFailure(throwable -> log.error("❌ Failed to deploy verticle", throwable.getCause()));
